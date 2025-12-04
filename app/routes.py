92c4a660e1378e8e514bae133cc41abdc36360e3
beyuphan <117213326+beyuphan@users.noreply.github.com>
2025-12-04 14:17:32 +0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,116 @@
+from flask import render_template, flash, redirect, url_for, request
+from app import app
+from app.forms import LoginForm, ForgetPasswordForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from app.forms import EditProfileForm
+from urllib.parse import urlsplit
+from datetime import datetime, timezone
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user = {'username' : "Yüce Eyüphan BİNİCİ"}
+    posts = [
+        {
+            'author': {'username': 'Ahmet'},
+            'body': 'Beautiful day in Samsun!'
+        },
+        {
+            'author': {'username': 'Hami'},
+            'body': 'The Avengers movie was so cool!'
+        }
+
+    ]
+
+    return render_template('index.html', title='Home', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+# end def
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm() 
+    
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Tebrikssssss!')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password' )
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/forget', methods = ['GET', 'POST'])
+def forget():
+    form = ForgetPasswordForm()
+    return render_template('forget.html', title='Forget Password', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user,
+        'body': 'Deneme gönderi #1'},
+
+        {'author': user,
+        'body': 'Deneme gönderi #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Değişiklikler kaydedildi.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
