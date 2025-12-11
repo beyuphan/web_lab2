cf2bf631191bd6a75bfd0eb2302dfbd01bd76835
beyuphan <117213326+beyuphan@users.noreply.github.com>
2025-12-11 14:51:49 +0300

    son

--- app/forms.py
+++ app/forms.py
@@ -45,3 +45,13 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     konum = StringField('Konum', validators=[Length(min=0, max=100)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(User.username == username.data))
+            if user is not None:
+                raise ValidationError('Bu kullanıcı adı zaten kullanılıyor. Lütfen başka bir ad seçin.')
